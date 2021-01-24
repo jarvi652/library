f91a0136832af7114efdd94c0d8a68d814a65c5c
jarvi652 <jarviskatana@gmail.com>
2021-01-24 13:59:18 +0300

    'final'

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
 const submit = document.querySelector('.btn')
-submit.addEventListener('click' ,addBooktoLibrary)
+submit.addEventListener('click', addBooktoLibrary)
 const popUpForm = document.querySelector('.grid_container')
 popUpForm.style.display = 'none'
 const newForm = document.querySelector('.butn')
 newForm.addEventListener('click', e=>{
     popUpForm.style.display = 'block'
 })
+const title = document.getElementById('title')
+const author = document.getElementById('author')
+const pages = document.getElementById('pages')
+const status = document.getElementById('select')
+const content = document.querySelector('.content')
+
+
+
 let library =[]
-function Book(title,author,pages,read,notread){
+function Book(title,author,pages,status){
     this.title = title
     this.author =author
     this.pages = pages
-    this.read = read
-    this.notread =notread
+    this.status = status
 }
 var newBook
-function addBooktoLibrary(title,author,pages,read,notread){
+
+function addBooktoLibrary(event){
+    event.preventDefault()
     popUpForm.style.display = 'none'
-    newBook = new Book(title,author,pages,read,notread)
-    library.push(newBook)
-    return library
+    //differentiating between read and notread
+    if(title.value === '' || author.value ==='' || pages.value === ''|| status.value===''){
+        alert('Please fill in the form')
+    }
+    else if(status.value === 'read'){
+        newBook = new Book(title.value,author.value,pages.value,status.value)
+        library.push(newBook)
+        console.log(library)
+        //adding the form to the html elements 
+        var ourHTML = '<div class="content-box"><div class="col-4">Title:<p>%title%</p></div><div class="col-4">Author:<p>%author%</p></div><div class="col-4">Status:<p>%status%</p></div></div>'
+        var ournewHtml
+        ournewHtml = ourHTML.replace('%title%', title.value)
+        ournewHtml = ournewHtml.replace('%author%',author.value)
+        ournewHtml = ournewHtml.replace('%status%', status.value)
+        var htmlObject = document.createElement('div')
+        htmlObject.classList.add('content-box')
+        htmlObject.innerHTML=ournewHtml
+        content.insertAdjacentElement('afterend',htmlObject)
+        
+
+
+    }else if(status.value ==='notread'){
+        newBook = new Book(title.value,author.value,pages.value,status.value)
+        library.push(newBook)
+        //adding the form to html elements
+        var ourHTML = '<div class="content-box"><div class="col-4">Title:<p>%title%</p></div><div class="col-4">Author:<p>%author%</p></div><div class="col-4">Status:<p>%status%</p></div></div>'
+        var ournewHtml
+        ournewHtml = ourHTML.replace('%title%', title.value)
+        ournewHtml = ournewHtml.replace('%author%',author.value)
+        ournewHtml = ournewHtml.replace('%status%', status.value)
+        var htmlObject = document.createElement('div')
+        htmlObject.classList.add('content-box')
+        htmlObject.innerHTML=ournewHtml
+        content.insertAdjacentElement('beforeend',htmlObject)
+    }  
+    
 }
+
+
+
